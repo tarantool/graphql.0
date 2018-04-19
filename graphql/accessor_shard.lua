cca757a412dfed4e809f0a88dbeef3abe3da725d
Alexander Turenko <alexander.turenko@tarantool.org>
2018-04-19 10:59:14 +0300

    Support shard-2.1
    Part of #111.

--- graphql/accessor_shard.lua
+++ graphql/accessor_shard.lua
@@ -2,6 +2,7 @@
 --- (@{accessor_general}) behaves as shard accessor and provides the
 --- `accessor_shard.new` function to create a new shard data accessor instance.
 
+local json = require('json')
 local shard = require('shard')
 local accessor_general = require('graphql.accessor_general')
 
@@ -9,15 +10,22 @@ local accessor_shard = {}
 
 local LIMIT = 100000 -- XXX: we need to raise an error when a limit reached
 
+local function shard_check_error(func_name, result, err)
+    if result ~= nil then return end
+    error(('%s: %s'):format(func_name, json.encode(err)))
+end
+
 -- Check whether a collection (it is sharded space for that accessor) exists.
 local function is_collection_exists(collection_name)
+    local func_name = 'accessor_shard.is_collection_exists'
     local exists
     for _, zone in ipairs(shard.shards) do
         for _, node in ipairs(zone) do
-            local cur = shard:space_call(collection_name, node,
+            local cur, err = shard:space_call(collection_name, node,
                 function(space_obj)
                     return space_obj ~= nil
                 end)
+            shard_check_error(func_name, cur, err)
             assert(exists == nil or cur == exists,
                 ('space "%s" exists on some shards, ' ..
                 'but does not on others'):format(collection_name))
@@ -31,13 +39,15 @@ end
 --- determining whether the index exists within a shard cluster is
 --- not-so-trivial as for local spaces.
 local function is_index_exists(collection_name, index_name)
+    local func_name = 'accessor_shard.is_index_exists'
     local exists
     for _, zone in ipairs(shard.shards) do
         for _, node in ipairs(zone) do
-            local cur = shard:space_call(collection_name, node,
+            local cur, err = shard:space_call(collection_name, node,
                 function(space_obj)
                     return space_obj.index[index_name] ~= nil
                 end)
+            shard_check_error(func_name, cur, err)
             assert(exists == nil or cur == exists,
                 ('index "%s" of space "%s" exists on some shards, ' ..
                 'but does not on others'):format(index_name, collection_name))
@@ -57,10 +67,12 @@ local function get_index(collection_name, index_name)
     local index = setmetatable({}, {
         __index = {
             pairs = function(self, value, opts)
+                local func_name = 'accessor_shard.get_index.<index>.pairs'
                 local opts = opts or {}
                 opts.limit = opts.limit or LIMIT
-                local tuples = shard:secondary_select(collection_name,
+                local tuples, err = shard:secondary_select(collection_name,
                     index_name, opts, value, 0)
+                shard_check_error(func_name, tuples, err)
                 local cur = 1
                 local function gen()
                     if cur > #tuples then return nil end
